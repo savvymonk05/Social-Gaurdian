2a6a597a49b678168e864445e445c0ab1608fdda
savvymonk05 <himanshusingh20023@gmail.com>
2025-02-07 20:06:17 +0530

    Added new feature

--- Social-Gaurdian/src/components/Addictiontest.jsx
+++ Social-Gaurdian/src/components/Addictiontest.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Cards from "../components/Questioncard"
 
 function Addictiontest() {
   return (
-    <div>Hi this Addiction</div>
+    <div>
+    <h1 className='text-center relative top-[50px] font-archivo text-4xl leading-[48px] font-bold text-neutral-900'>
+      GIVE THE ADDICTION TEST BY ANSWERING THE BELOW QUESTIONS 
+    </h1>
+    <Cards/>
+   </div>
   )
 }
 
---
+++ Social-Gaurdian/src/components/Questioncard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function QuestionCard() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-[808px] h-[444px] bg-[#F2F2FD] rounded-tl-[2px] rounded-bl-[2px] flex justify-center items-center h-screen shadow-lg">
+        {/* Content here */}
+      </div>
+    </div>
+  );
+}
+
+export default QuestionCard;
