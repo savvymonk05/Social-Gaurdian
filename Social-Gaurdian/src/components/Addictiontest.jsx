5b0fcb8f670570760f8945153e02a08f5f4318b2
savvymonk05 <himanshusingh20023@gmail.com>
2025-02-08 22:28:06 +0530

    Added footer to Addictiontest

--- Social-Gaurdian/src/components/Addictiontest.jsx
+++ Social-Gaurdian/src/components/Addictiontest.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Cards from "../components/Questioncard"
+import Footer from "../components/Footer"
 
 function Addictiontest() {
   return (
     <div>
     <h1 className='text-center relative top-[50px] font-archivo text-4xl leading-[48px] font-bold text-neutral-900'>
-      GIVE THE ADDICTION TEST BY ANSWERING THE BELOW QUESTIONS 
+      GIVE THE ADDICTION TEST BY ANSWERING THE BELOW QUESTIONS!!
     </h1>
     <Cards/>
+    <Footer/>
    </div>
   )
 }
